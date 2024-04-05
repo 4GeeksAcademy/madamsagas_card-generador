a94b33d6d25c04fef44062aff4b05f0fb45d0e86
MadamSagas <141463441+MadamSagas@users.noreply.github.com>
2024-04-05 16:20:30 +0000

    agregue el codigo JS en el archivo app.js para crear el generador de cartas, tambien agregue el codigo correspondiente al index.html, y a la hoja de estilos styles.css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
 /* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardPocker = () => {
+    let randomNumber = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomIcon = [
+      { symbol: "♦", color: "red" },
+      { symbol: "♥", color: "red" },
+      { symbol: "♠", color: "black" },
+      { symbol: "♣", color: "black" }
+    ];
+    let randomNumberShow = Math.floor(Math.random() * randomNumber.length);
+    let randomIconShow = Math.floor(Math.random() * randomIcon.length);
+    return {
+      randomNumber: randomNumber[randomNumberShow],
+      randomIcon: randomIcon[randomIconShow]
+    };
+  };
+
+  let randomCard = cardPocker();
+
+  let iconCard = randomCard.randomIcon;
+  document.querySelector(".div-center").innerText = randomCard.randomNumber;
+  document.querySelectorAll(".div-right, .div-left").forEach(value => {
+    value.innerText = iconCard.symbol;
+    value.classList.add(iconCard.color);
+  });
 };
